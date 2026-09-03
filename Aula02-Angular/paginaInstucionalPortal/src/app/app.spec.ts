26a53199d10419fdb16d3dbfeec17b3d4f120801
Gabriel-cruz209 <gc572640@gmail.com>
2026-09-03 16:44:32 -0300

    Atualização atividade

--- Aula02-Angular/paginaInstucionalPortal/src/app/app.spec.ts
+++ Aula02-Angular/paginaInstucionalPortal/src/app/app.spec.ts
@@ -15,10 +15,10 @@ describe('App', () => {
     expect(app).toBeTruthy();
   });
 
-  it('should render title', async () => {
+  it('should render the SENAI portal header', async () => {
     const fixture = TestBed.createComponent(App);
     await fixture.whenStable();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, paginaInstucionalPortal');
+    expect(compiled.querySelector('.brand-name')?.textContent).toContain('SENAI');
   });
 });
